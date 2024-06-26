f404d5fd66e42009020f4034f0989bad133daab2
Arnd Flemmer <arnd.flemmer@gmx.de>
2024-06-26 09:01:08 +0200

    CSV_DKB

---
+++ core/CSV_DKB.php
@@ -0,0 +1,28 @@
+<?php
+
+require "CSVReader.php";
+
+
+class CSV_DKB extends CSVReader
+{
+    protected function convertData()
+    {
+        foreach( $this->csvData as $row )
+        {
+            print_r( $row );
+            $newRow = [];
+            $newRow["date"] = $row["Buchungsdatum"];
+            $newRow["description"] = $row["Verwendungszweck"];
+            $newRow["amount"] = $row["Betrag (€)"];
+            $newRow["client"] = $row["Zahlungsempfänger*in"];
+            $newRow["iban"] = $row["IBAN"];
+            $newRow["bic"] = "";
+            $newRow["reference"] = $row["Kundenreferenz"];
+            $newRow["category"] = "";
+            $newRow["costcenter"] = "";
+            $newRow["account"] = "DKB";
+
+            $this->data[] = $newRow;
+        }
+    }
+}
